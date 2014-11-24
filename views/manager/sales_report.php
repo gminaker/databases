03f7ac51a3413533a8ff52277bd4b4bcc98b4033
thpierce <tpierce@interchange.ubc.ca>
2014-11-23 22:11:46 -0800

    sales_report is mostly done, just need another person to double check everything is quite happy!

--- views/manager/sales_report.php
+++ views/manager/sales_report.php
@@ -18,12 +18,12 @@ function generateDailySalesReport($date){
 	
 	$date = date('Y-m-d', strtotime($date));
 	
-	$results = $connection->query("	SELECT i.it_upc, sum(pi.pi_quantity)
+	$results = $connection->query("	SELECT i.it_upc, i.category, i.price, sum(pi.pi_quantity), i.price*sum(pi.pi_quantity)
 									FROM purchase p, purchaseItem pi, item i 
 									WHERE p.p_receiptId = pi.pi_receiptId
 									AND pi.pi_upc = i.it_upc
 									AND p.p_date = '$date'
-									GROUP BY i.it_upc");
+									GROUP BY i.it_upc, i.category, i.price;");
 									
 	if(!$results){
 		printf("Error: %s\n", $connection->error);
@@ -31,17 +31,65 @@ function generateDailySalesReport($date){
  	
  	if($results->num_rows == 0){
 	 	print '<tr><td colspan=5>No Items Found</td></tr>';
- 	}
+ 	} else {
+ 		print '<tr><td colspan=5>Report for: '.$date.'</td></tr><tr></tr>';
+	 	print '<tr>	<td>UPC</td>
+	 				<td>Category</td>
+ 				<td>Unit Price</td>
+ 				<td>Units</td>
+ 				<td>Total Value</td>
+ 			</tr>';
 
- 	$i = 0;
-	while($row = $results->fetch_assoc()) {
-	    print '<tr>';
-		    print '<td>'.$row["it_upc"].'</td>';
-		    print '<td>'.$row["pi_quantity"].'</td>';
-	    print '</tr>';
+ 		$i = 0;
+ 		$category = "";
+ 		$totalunits = 0;
+ 		$totalvalue = 0.00;
+ 		$gtotalunits = 0;
+ 		$gtotalvalue = 0.00;
+
+		while($row = $results->fetch_assoc()) {
+
+		    print '<tr>';
+			    print '<td>'.$row["it_upc"].'</td>';
+			    print '<td>'.$row["category"].'</td>';
+			    print '<td>'.$row["price"].'</td>';
+			    print '<td>'.$row["sum(pi.pi_quantity)"].'</td>';
+		    	print '<td>'.$row["i.price*sum(pi.pi_quantity)"].'</td>';
+	    	print '</tr>';
 	    
-	    $i++;
-	}  
+	    	$totalunits += floatval($row["sum(pi.pi_quantity)"]);
+ 			$totalvalue += floatval($row["i.price*sum(pi.pi_quantity)"]);
+ 			$gtotalunits += floatval($row["sum(pi.pi_quantity)"]);
+ 			$gtotalvalue += floatval($row["i.price*sum(pi.pi_quantity)"]);
+
+ 			if ($row["category"] != $category){
+				print '<tr>';
+		   			print '<td></td>';
+		   			print '<td>Total</td>';
+		    		print '<td></td>';
+		    		print '<td>'.$totalunits.'</td>';
+		    		print '<td>'.$totalvalue.'</td>';
+	    		print '</tr>';
+
+				$category = $row["category"];
+				$totalunits = 0.00;
+ 				$totalvalue = 0.00;
+	 		}
+		    
+
+	    	$i++;
+		}
+
+		if ($category != ""){
+			print '<tr>';
+				print '<td></td>';
+				print '<td>Total Daily Sales</td>';
+		    	print '<td></td>';
+		    	print '<td>'.$gtotalunits.'</td>';
+		    	print '<td>'.$gtotalvalue.'</td>';
+	    	print '</tr>';
+		} 
+	}
 
 	$results->free();
 }
