ed8186a4999c723c2eb44ad9f6674712d0536442
Mike Fink <mike.g.fink@gmail.com>
2014-11-24 20:29:36 -0800

    Tried to implement expected date

--- views/customer/purchase_items.php
+++ views/customer/purchase_items.php
@@ -15,27 +15,70 @@
   if ($_SERVER["REQUEST_METHOD"] == "POST") {
  		
       if (isset($_POST["purchase_items"]) && $_POST["purchase_items"] == "SUBMIT") {
-		checkValsThenInsertIntoDB($_POST['user_ccno'],
-		      					  $_POST['user_ccex']);
+		checkValsThenInsertIntoDB(	$_POST['user_id'],
+		      						$_POST['user_pass'],
+		      						$_POST['user_ccno'],
+		      						$_POST['user_ccex'],
+		      						$_POST);
       }
  }
  
  
- function checkValsThenInsertIntoDB($cc_no, $cc_ex){
+ function checkValsThenInsertIntoDB($user_id, $user_pass, $cc_no, $cc_ex, $all){
 	 
-	$msg = checkValues($cc_no, $cc_ex); 
+	$msg = checkValues($user_id, $user_pass, $cc_no, $cc_ex, $all); 
 	$expected_date = calculateExpectedDate();
 	
 	if($msg){
 		printf("%s", $msg);
 	}else{
-		insertIntoDB($cc_no, $cc_ex, $expected_date);
+		insertIntoDB($user_id, $user_pass, $cc_no, $cc_ex, $expected_date, $all);
 	}
 }
 
 function calculateExpectedDate(){
 	// TODO make a funcion that returns the expected delivery date for the order. 
-	return 0;
+	$deliveries_per_day = 5;
+	$result = getMaxExpectedDate();
+	
+	if ($result['count'] < 5) {
+		$expectedDate = $result['expectedDate'];
+	} else {
+		$expectedDate = $result['expectedDate']->add(new DateInterval('P1D'));
+	}
+	var_dump($expectedDate);
+	return $expectedDate;
+}
+
+function getMaxExpectedDate(){
+	global $connection;
+	$stmt = $connection->query("SELECT expectedDate
+								FROM purchase
+								WHERE expectedDate = (
+								SELECT MAX(expectedDate) 
+								FROM purchase);");
+	    
+	if($stmt->error) {       
+		printf("<b>Error: %s.</b>\n", $stmt->error);
+		return;
+	}
+	     
+	$stmt->store_result();
+	$count = $stmt->num_rows;
+
+	$stmt->bind_result($expectedDate);
+		
+	if($count == 0){
+		print("Sorry bud, no expected dates.\n");
+		exit();
+	} 
+	$stmt->fetch();
+	
+	$result = array();
+	$result['expectedDate'] = $expectedDate;
+	$result['count'] = $count;
+	//var_dump($item_name);
+	return $result;
 }
 
 /**
@@ -49,7 +92,7 @@ function calculateExpectedDate(){
  * @param string $all
  *
  */
-function checkValues($cc_no, $cc_ex){
+function checkValues($user_id, $user_pass, $cc_no, $cc_ex, $all){
 	
 	//TODO check user id & password.
 	return null;
@@ -67,13 +110,13 @@ function checkValues($cc_no, $cc_ex){
  * @param string $all
  *
  */
-function insertIntoDB($cc_no, $cc_ex, $expected_date){
+function insertIntoDB($user_id, $user_pass, $cc_no, $cc_ex, $expected_date, $all){
 
 	global $connection;
  	$stmt = $connection->prepare("INSERT INTO purchase (p_date, p_cid, cardNo, expiryDate, expectedDate) VALUES (?,?,?,?,?)");
     
     // Bind the title and pub_id parameters, 'sss' indicates 3 strings
-    $stmt->bind_param("sssss", date('Y-m-d h:i:s', time()), $_SESSION['user_id'], $cc_no, $cc_ex, $expected_date);
+    $stmt->bind_param("sssss", date('Y-m-d h:i:s', time()), $user_id, $cc_no, $cc_ex, $expected_date);
     
     // Execute the insert statement
     $stmt->execute();
@@ -87,11 +130,11 @@ function insertIntoDB($cc_no, $cc_ex, $expected_date){
     
     $receiptId = $stmt->insert_id;
     
-    if(isset($_SESSION['cart'])){
-	    foreach($_SESSION['cart'] as $key => $value) {
+    if(isset($_POST['purchase'])){
+	    foreach($_POST['purchase'] as $key => $value) {
 		    
-			    $upc = $key;
-			    $qty = $value;
+			    $upc = $value['upc'];
+			    $qty = $value['qty'];
 			    
 				if ($qty > 0){
 				  	
@@ -118,4 +161,79 @@ function insertIntoDB($cc_no, $cc_ex, $expected_date){
     
  }
  
- 	 
+ 
+
+ function getAllItemRows(){
+	global $connection;
+ 	$results = $connection->query("SELECT * FROM item");
+ 	
+ 	if($results->num_rows == 0){
+	 	print '<tr><td colspan=9>No Items Found</td</tr>';
+ 	}
+
+ 	$i = 0;
+	while($row = $results->fetch_assoc()) {
+	    print '<tr>';
+		    print '<td><input type="text" size="5" name="purchase['.$i.'][qty]"></td>';
+		    print '<input type="hidden" size="5" name="purchase['.$i.'][upc]" value="'.$row["it_upc"].'">';
+		    print '<td>'.$row["it_upc"].'</td>';
+		    print '<td>'.$row["it_title"].'</td>';
+		    print '<td>'.$row["type"].'</td>';
+		    print '<td>'.$row["category"].'</td>';
+		    print '<td>'.$row["company"].'</td>';
+		    print '<td>'.$row["year"].'</td>';
+		    print '<td>'.$row["price"].'</td>';
+		    print '<td>'.$row["stock"].'</td>';
+	    print '</tr>';
+	    
+	    $i++;
+	}  
+
+	$results->free();
+ }
+ 
+ ?>
+ 
+ <form method=post name="purchase_item_form">
+ <input type="hidden" name="purchase_items" value="SUBMIT">
+ <table>
+	 <tr> 
+		 <td>User ID:</td>
+		 <td><input type="text" name="user_id"></td>
+	 </tr>
+	 <tr>
+		 <td>User Password</td>
+		 <td><input type="password" name="user_pass"></td>
+	 </tr>
+	 <tr>
+		 <td>Credit Card Number</td>
+		 <td><input type="text" name="user_ccno"></td>
+	 </tr>
+	 <tr>
+		 <td>Credit Card Expiry</td>
+		 <td><input type=date name="user_ccex"></td>
+	 </tr>
+ </table>
+ 
+ <table border="1">
+	 <tr>
+		 <th>QTY</th>
+		 <th>UPC</th>
+		 <th>Title</th>
+		 <th>Type</th>
+		 <th>Category</th>
+		 <th>Company</th>
+		 <th>Year</th>
+		 <th>Price</th>
+		 <th>Stock</th>
+	 </tr>
+	 
+	 <?php getAllItemRows(); ?>
+	 
+	 <tr>
+		 <td colspan=8></td>
+		 <td><input type=submit value="Place Order"></td>
+	 </tr>
+ </table>
+ </form>
+	 
